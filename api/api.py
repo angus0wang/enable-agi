214bf1c73eb296931e2aa56e15ab58c330407949
kailiu <angus0wang@163.com>
2024-05-21 22:15:49 +0800

    stream mode

--- api/api.py
+++ api/api.py
@@ -1,3 +1,4 @@
+from fastapi.responses import StreamingResponse
 from langchain_community.llms import Ollama
 
 from fastapi import FastAPI
@@ -8,5 +9,4 @@ app = FastAPI()
 @app.get("/basic/chat")
 def basic_chat(input_text:str):
     llm = Ollama(model="llama3:8b-instruct-q4_K_M")
-    return llm.invoke(input_text)
-
+    return StreamingResponse(llm.stream(input_text))
